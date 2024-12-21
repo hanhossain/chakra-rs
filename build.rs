a12cd277c979f445bacf5c620bb9dd790a2aeca5
Han Hossain <hanhossain@outlook.com>
2024-12-21 12:05:22 -0600

    use clang

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ fn main() {
     cmake::Config::new("chakracore-cxx")
         .generator("Ninja")
         .define("DISABLE_JIT", "ON")
+        .define("CMAKE_CXX_COMPILER", "clang++")
+        .define("CMAKE_C_COMPILER", "clang")
         .build_target("ch")
         .build();
 }
