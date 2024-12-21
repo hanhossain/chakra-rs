08e22a9df07c90c2103a580e7d3881c78ea37b0d
Han Hossain <hanhossain@outlook.com>
2024-12-21 12:27:29 -0600

    always use clang

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
+    let mut cc_config = cc::Build::new();
+    if !cc_config.get_compiler().is_like_clang() {
+        cc_config.compiler("clang");
+    }
+
     cmake::Config::new("chakracore-cxx")
+        .init_c_cfg(cc_config)
         .generator("Ninja")
         .define("DISABLE_JIT", "ON")
         .define("CMAKE_CXX_COMPILER", "clang++")
