ea669a9a60057465ddcf09f636ccd907e0a579ee
Han Hossain <hanhossain@outlook.com>
2024-12-22 01:59:35 -0600

    migrate With

--- tests/basics.rs
+++ tests/basics.rs
@@ -8,3 +8,12 @@ fn accessors() {
     };
     common::run_test(&test);
 }
+
+#[test]
+fn with() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/With.js",
+        baseline_path: "chakracore-cxx/test/Basics/With.baseline",
+    };
+    common::run_test(&test);
+}
