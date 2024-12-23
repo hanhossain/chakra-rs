eda198ffb3017d54c025105539113df129b1d551
Han Hossain <hanhossain@outlook.com>
2024-12-23 00:01:35 -0600

    cleanup

--- tests/basics.rs
+++ tests/basics.rs
@@ -1,134 +1,6 @@
 mod common;
 
 #[test]
-fn accessors_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/Accessors.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/Accessors.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn with_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/With.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/With.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn with2_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/With2.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/With2.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn defprop_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/DefProp.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/DefProp.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn scopedaccessors_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/scopedaccessors.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/scopedaccessors.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn flags_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/flags.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/flagsES5.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn inlinecache_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/inlinecache.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/inlinecache.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn scan_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/scan.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/scan.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn enum_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/enum.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/enum.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn with3_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/with3.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/with3.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn bug650104_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/bug650104.js",
-        baseline_path: Some("chakracore-cxx/test/Basics/bug650104.baseline"),
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn delete_and_re_add_non_extensible_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/DeleteAndReAddNonExtensible.js",
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
-fn cross_site_accessor_main_js() {
-    let test = common::Test {
-        source_path: "chakracore-cxx/test/Basics/cross_site_accessor_main.js",
-        ..Default::default()
-    };
-    common::run_test(&test);
-}
-
-#[test]
 fn array_js() {
     let test = common::Test {
         source_path: "chakracore-cxx/test/Basics/Array.js",
@@ -147,6 +19,15 @@ fn script_function_to_strings_js() {
     };
     common::run_test(&test);
 }
+
+// TODO (hanhossain): migrate test
+// <test>
+//   <default>
+//     <files>DomProperties.js</files>
+//     <compile-flags>-Intl-</compile-flags>
+//     <tags>Slow</tags>
+//   </default>
+// </test>
 
 #[test]
 fn array_concat_js() {
@@ -369,6 +250,16 @@ fn undefined_vs_null_js() {
 }
 
 #[test]
+fn with_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/With.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/With.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
 fn with_js_defer_parse() {
     let test = common::Test {
         source_path: "chakracore-cxx/test/Basics/With.js",
@@ -409,6 +300,16 @@ fn with_bug940841_2_js() {
 }
 
 #[test]
+fn with2_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/With2.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/With2.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
 fn witheval_js() {
     let test = common::Test {
         source_path: "chakracore-cxx/test/Basics/witheval.js",
@@ -439,11 +340,119 @@ fn delete_property1_js() {
 }
 
 #[test]
+fn delete_and_re_add_non_extensible_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/DeleteAndReAddNonExtensible.js",
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn accessors_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/Accessors.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/Accessors.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn defprop_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/DefProp.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/DefProp.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn scopedaccessors_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/scopedaccessors.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/scopedaccessors.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn flags_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/flags.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/flagsES5.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
 fn branching_js() {
     let test = common::Test {
         source_path: "chakracore-cxx/test/Basics/Branching.js",
         baseline_path: Some("chakracore-cxx/test/Basics/Branching.baseline"),
         compile_flags: vec!["-Intl-"],
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn inlinecache_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/inlinecache.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/inlinecache.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn scan_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/scan.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/scan.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn enum_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/enum.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/enum.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn with3_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/with3.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/with3.baseline"),
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn cross_site_accessor_main_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/cross_site_accessor_main.js",
+        ..Default::default()
+    };
+    common::run_test(&test);
+}
+
+#[test]
+fn bug650104_js() {
+    let test = common::Test {
+        source_path: "chakracore-cxx/test/Basics/bug650104.js",
+        baseline_path: Some("chakracore-cxx/test/Basics/bug650104.baseline"),
+        ..Default::default()
     };
     common::run_test(&test);
 }
@@ -457,6 +466,44 @@ fn special_symbol_capture_js() {
     };
     common::run_test(&test);
 }
+
+// TODO (hanhossain): migrate test
+// <test>
+//   <default>
+//     <files>VerifyParserState.js</files>
+//     <baseline>VerifyParserState.baseline</baseline>
+//     <compile-flags>-UseParserStateCache -ParserStateCache -Force:DeferParse -Trace:CreateParserState</compile-flags>
+//     <tags>exclude_test</tags>
+//   </default>
+// </test>
+
+// TODO (hanhossain): migrate test
+// <test>
+//   <default>
+//     <files>VerifySkipNestedDeferred.js</files>
+//     <baseline>VerifySkipNestedDeferred.baseline</baseline>
+//     <compile-flags>-UseParserStateCache -ParserStateCache -Force:DeferParse -Trace:SkipNestedDeferred</compile-flags>
+//     <tags>exclude_test,exclude_dynapogo</tags>
+//   </default>
+// </test>
+
+// TODO (hanhossain): migrate test
+// <test>
+//   <default>
+//     <files>bug_os17542375.js</files>
+//     <compile-flags>-UseParserStateCache -ParserStateCache -Force:DeferParse -pageheap:2</compile-flags>
+//     <tags>exclude_test</tags>
+//   </default>
+// </test>
+
+// TODO (hanhossain): migrate test
+// <test>
+//   <default>
+//     <files>bug_os16855035.js</files>
+//     <compile-flags>-UseParserStateCache -ParserStateCache -Force:DeferParse -Force:Redeferral -CollectGarbage</compile-flags>
+//     <tags>exclude_test</tags>
+//   </default>
+// </test>
 
 #[test]
 fn float_comparison_js() {
