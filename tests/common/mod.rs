b10691609357ab7c942312436352c31e9546d340
Han Hossain <hanhossain@outlook.com>
2024-12-22 21:40:49 -0600

    append stdout and stderr to same string for assertion

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -12,15 +12,17 @@ pub fn run_test(test: &Test) {
     let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let source = manifest_dir.join(test.source_path);
     let baseline = manifest_dir.join(test.baseline_path);
-
     let out_dir = PathBuf::from(env!("OUT_DIR"));
+
     let mut ch = Command::new(out_dir.join("build/ch"));
     ch.arg(source)
         .arg("-ExtendedErrorStackForTestHost")
         .arg("-BaselineMode");
-
     let output = ch.output().unwrap();
-    let out = String::from_utf8_lossy(&output.stdout);
+
+    let mut out = String::from_utf8(output.stdout).unwrap();
+    let err = std::str::from_utf8(&output.stderr).unwrap();
+    out.push_str(err);
 
     let actual = out
         .lines()
@@ -31,8 +33,8 @@ pub fn run_test(test: &Test) {
         .lines()
         .map(|s| trim_carriage_return(s))
         .collect::<Vec<_>>();
+
     assert_eq!(actual, expected);
-    assert_eq!(output.stderr, b"");
     assert!(output.status.success());
 }
 
