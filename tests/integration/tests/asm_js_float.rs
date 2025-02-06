ab80d1bdcf45b4a08e269a70c87f001d53fff4d5
Han Hossain <hanhossain@outlook.com>
2025-02-06 09:59:28 -0600

    fix unix tests

--- tests/integration/tests/asm_js_float.rs
+++ tests/integration/tests/asm_js_float.rs
@@ -3,7 +3,7 @@ use crate::common::Variant;
 use rstest::rstest;
 use std::collections::HashSet;
 
-const DIRECTORY: &str = "chakracore-cxx/test/AsmJsFloat";
+const DIRECTORY: &str = "chakracore-cxx/test/AsmJSFloat";
 
 #[rstest]
 #[case::interpreted(Variant::Interpreted)]
--- tests/integration/tests/asm_js_parser.rs
+++ tests/integration/tests/asm_js_parser.rs
@@ -3,7 +3,7 @@ use crate::common::Variant;
 use rstest::rstest;
 use std::collections::HashSet;
 
-const DIRECTORY: &str = "chakracore-cxx/test/AsmJsParser";
+const DIRECTORY: &str = "chakracore-cxx/test/ASMJSParser";
 
 #[rstest]
 #[case::interpreted(Variant::Interpreted)]
