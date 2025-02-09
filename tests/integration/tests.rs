343ba8d63f54254616270d6e79eba2118dc7ec68
Han Hossain <hanhossain@outlook.com>
2025-02-09 00:05:48 -0600

    remove ttd todo

--- tests/integration/tests.rs
+++ tests/integration/tests.rs
@@ -72,9 +72,7 @@ mod tagged_integers;
 mod test262;
 mod test_loop;
 mod try_catch;
-// TODO (hanhossain): fix tests
 mod ttbasic;
-// TODO (hanhossain): fix tests
 mod ttexecute_basic;
 mod typed_array;
 mod unified_regex;
