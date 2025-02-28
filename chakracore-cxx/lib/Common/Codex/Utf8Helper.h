471139156230c8b893d60c19cb8f27387b9e73e4
Han Hossain <hanhossain@outlook.com>
2025-02-28 02:31:14 -0600

    Codex

--- chakracore-cxx/lib/Common/Codex/Utf8Helper.h
+++ chakracore-cxx/lib/Common/Codex/Utf8Helper.h
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 #pragma once
+#include "PlatformAgnostic/CommonPal.h"
 #include "Utf8Codex.h"
 
 namespace utf8
