efafaf3d0fde38d73cbe8f619d31e1371ed8bec8
Han Hossain <hanhossain@outlook.com>
2025-03-21 19:58:50 -0500

    CommonMin - remove DataStructures/BitVector

--- chakracore-cxx/lib/Common/Memory/VirtualAllocWrapper.h
+++ chakracore-cxx/lib/Common/Memory/VirtualAllocWrapper.h
@@ -4,6 +4,8 @@
 //-------------------------------------------------------------------------------------------------------
 #pragma once
 
+#include "DataStructures/BitVector.h"
+
 namespace Memory
 {
 
