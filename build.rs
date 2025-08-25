d973fe34d8d508c66c290e27d3bca2cd69b102ad
Han Hossain <hanhossain@outlook.com>
2025-08-25 13:53:45 -0500

    remove windows build

--- build.rs
+++ build.rs
@@ -1,18 +1,9 @@
-use std::path::PathBuf;
-
 fn main() {
     // only compile c++ when asked to, this allows cargo check to skip it until we need it.
     if cfg!(feature = "compile-cpp") {
         let debug: bool = std::env::var("DEBUG").unwrap().parse::<bool>().unwrap();
         let optimized = cfg!(feature = "optimized-tests");
-
-        let target = std::env::var("TARGET").unwrap();
-        if target.contains("windows") {
-            build_msvc(optimized, debug);
-        } else {
-            #[cfg(unix)]
-            build_cmake(optimized, debug);
-        }
+        build_cmake(optimized, debug);
     }
 
     // set rstest timeout to 60s
@@ -24,7 +15,6 @@ fn main() {
     }
 }
 
-#[cfg(unix)]
 fn build_cmake(optimized: bool, debug: bool) {
     let mut cc_config = cc::Build::new();
     if !cc_config.get_compiler().is_like_clang() {
@@ -54,30 +44,4 @@ fn build_cmake(optimized: bool, debug: bool) {
     }
 
     config.build();
-}
-
-fn build_msvc(optimized: bool, debug: bool) {
-    let configuration = match (optimized, debug) {
-        (false, true) => "Debug",
-        (true, true) => "Test",
-        (_, false) => "Release",
-    };
-
-    let mut msbuild = std::process::Command::new("msbuild");
-    let sln = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
-        .join("chakracore-cxx/Build/Chakra.Core.sln");
-    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("build/");
-
-    let status = msbuild
-        .arg(format!("/p:Configuration={configuration}"))
-        .arg("/p:Platform=x64")
-        .arg(format!("/p:OutDir={}", out_dir.display()))
-        .arg("/m")
-        .arg(sln)
-        .status()
-        .unwrap();
-
-    if !status.success() {
-        panic!("Failed to build ChakraCore. Status: {status}");
-    }
 }
