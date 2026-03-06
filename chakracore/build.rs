a4b878ca9af63061a6d859642105754830e6639b
Han Hossain <hanhossain@outlook.com>
2026-03-06 00:12:41 -0600

    use virtual workspace

---
+++ chakracore/build.rs
@@ -0,0 +1,50 @@
+fn main() {
+    // only compile c++ when asked to, this allows cargo check to skip it until we need it.
+    if cfg!(feature = "compile-cpp") {
+        let debug: bool = std::env::var("DEBUG").unwrap().parse::<bool>().unwrap();
+        let optimized = cfg!(feature = "optimized-tests");
+        build_cmake(optimized, debug);
+    }
+
+    // set rstest timeout to 60s
+    println!("cargo::rustc-env=RSTEST_TIMEOUT=60");
+
+    println!("cargo::rustc-check-cfg=cfg(disable_jit)");
+    if !cfg!(target_arch = "x86_64") && cfg!(unix) {
+        println!("cargo::rustc-cfg=disable_jit");
+    }
+}
+
+fn build_cmake(optimized: bool, debug: bool) {
+    let mut cc_config = cc::Build::new();
+    if !cc_config.get_compiler().is_like_clang() {
+        cc_config.compiler("clang");
+    }
+
+    let build_type = match (optimized, debug) {
+        (false, true) => "Debug",
+        (true, true) => "RelWithDebInfo",
+        (_, false) => "Release",
+    };
+
+    let mut config = cmake::Config::new("../chakracore-cxx");
+    config
+        .init_c_cfg(cc_config)
+        .generator("Ninja")
+        .define("CMAKE_CXX_COMPILER", "clang++")
+        .define("CMAKE_C_COMPILER", "clang")
+        .profile(build_type)
+        .build_target("ch");
+
+    let target = std::env::var("TARGET").unwrap();
+    if target.contains("darwin") {
+        config
+            .define("DISABLE_JIT", "ON")
+            .define("ICU_INCLUDE_PATH", "/opt/homebrew/opt/icu4c@77/include");
+    }
+
+    config.always_configure(false);
+    config.build();
+
+    println!("cargo::rerun-if-changed=../chakracore-cxx/");
+}
---
+++ chakracore/tests/integration/tests/es6_generator_jit.rs
@@ -0,0 +1,327 @@
+use crate::common;
+use crate::common::Variant;
+use rstest::rstest;
+use std::collections::HashSet;
+
+const COMMON_TAGS: [&str; 1] = ["require_backend"];
+const DIRECTORY: &str = "../chakracore-cxx/test/es6GeneratorJit";
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn generator_jit_bugs_js(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "generator-jit-bugs.js",
+        compile_flags: vec!["-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn generator_jit_bugs_js2(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "generator-jit-bugs.js",
+        compile_flags: vec!["-JitES6Generators", "-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn generator_jit_bugs_js3(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "generator-jit-bugs.js",
+        compile_flags: vec![
+            "-JitES6Generators",
+            "-off:simplejit",
+            "-args",
+            "summary",
+            "-endargs",
+        ],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn generator_jit_bugs_js4(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "generator-jit-bugs.js",
+        compile_flags: vec![
+            "-JitES6Generators",
+            "-off:fulljit",
+            "-args",
+            "summary",
+            "-endargs",
+        ],
+        tags: HashSet::from(["require_backend", "exclude_nonative", "exclude_dynapogo"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn async_jit_bugs_js(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "async-jit-bugs.js",
+        compile_flags: vec!["-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn async_jit_bugs_js2(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "async-jit-bugs.js",
+        compile_flags: vec!["-JitES6Generators", "-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn async_jit_bugs_js3(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "async-jit-bugs.js",
+        compile_flags: vec![
+            "-JitES6Generators",
+            "-off:simplejit",
+            "-args",
+            "summary",
+            "-endargs",
+        ],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn async_jit_bugs_js4(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "async-jit-bugs.js",
+        compile_flags: vec![
+            "-JitES6Generators",
+            "-off:fulljit",
+            "-args",
+            "summary",
+            "-endargs",
+        ],
+        tags: HashSet::from(["require_backend", "exclude_nonative", "exclude_dynapogo"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn async_jit_bugs_js5(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "async-jit-bugs.js",
+        compile_flags: vec!["-off:fulljit", "-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative", "exclude_dynapogo"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn new_test_js(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "newtest.js",
+        compile_flags: vec!["-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn new_test_js2(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "newtest.js",
+        compile_flags: vec!["-off:simplejit", "-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn new_test_js3(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "newtest.js",
+        compile_flags: vec!["-off:fulljit", "-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative", "exclude_dynapogo"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn new_test_js4(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "newtest.js",
+        compile_flags: vec!["-off:fulljit", "-args", "summary", "-endargs"],
+        tags: HashSet::from(["require_backend", "exclude_nonative", "exclude_dynapogo"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(not(feature = "optimized-tests"))]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn jit_gen_loop_body_js(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "jit-gen-loop-body.js",
+        baseline_path: Some("jit-gen-loop-body.baseline"),
+        compile_flags: vec!["-testtrace:Backend"],
+        tags: HashSet::from([
+            "require_backend",
+            "exclude_test",
+            "exclude_nonative",
+            "exclude_dynapogo",
+        ]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn jit_gen_loop_body_js2(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "jit-gen-loop-body.js",
+        baseline_path: Some("jit-gen-loop-body-2.baseline"),
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(not(feature = "optimized-tests"))]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn jit_async_loop_body_js(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "jit-async-loop-body.js",
+        baseline_path: Some("jit-async-loop-body.baseline"),
+        compile_flags: vec!["-testtrace:Backend"],
+        tags: HashSet::from([
+            "require_backend",
+            "exclude_test",
+            "exclude_nonative",
+            "exclude_dynapogo",
+        ]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn jit_async_loop_body_js2(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "jit-async-loop-body.js",
+        baseline_path: Some("jit-async-loop-body-2.baseline"),
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(not(feature = "optimized-tests"))]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+fn jit_module_loop_body_js(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "jit-module-loop-body.js",
+        baseline_path: Some("jit-module-loop-body.baseline"),
+        compile_flags: vec!["-testtrace:Backend", "-module"],
+        tags: HashSet::from([
+            "require_backend",
+            "exclude_test",
+            "exclude_nonative",
+            "exclude_dynapogo",
+        ]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
+
+#[cfg(feature = "optimized-tests")]
+#[rstest]
+#[case::interpreted(Variant::Interpreted)]
+#[case::dynapogo(Variant::Dynapogo)]
+fn jit_module_loop_body_js2(#[case] variant: Variant) {
+    let test = common::Test {
+        directory: DIRECTORY,
+        source_path: "jit-module-loop-body.js",
+        baseline_path: Some("jit-module-loop-body-2.baseline"),
+        compile_flags: vec!["-module"],
+        tags: HashSet::from(["require_backend", "exclude_nonative"]),
+        ..Default::default()
+    };
+    common::run_test_variant(test, variant, COMMON_TAGS);
+}
