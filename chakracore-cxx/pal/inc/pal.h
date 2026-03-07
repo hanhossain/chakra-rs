b6db00671b00339ff125983ece4fd9cb1dbcb7d9
Han Hossain <hanhossain@outlook.com>
2026-03-06 21:17:45 -0600

    PAL_unlink

--- chakracore-cxx/pal/inc/pal.h
+++ chakracore-cxx/pal/inc/pal.h
@@ -2537,7 +2537,6 @@ GetSystemInfo(
 #define memmove       memmove_xplat
 #define mkstemp       PAL_mkstemp
 #define rename        PAL_rename
-#define unlink        PAL_unlink
 #define _wcstoui64    PAL__wcstoui64
 
 #endif // !PAL_STDCPP_COMPAT
