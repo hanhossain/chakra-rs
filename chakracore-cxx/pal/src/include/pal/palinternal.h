84ab5586bdcd381bbcfea8192ca41af97d80456d
Han Hossain <hanhossain@outlook.com>
2026-03-06 23:55:01 -0600

    PAL_wcstod

--- chakracore-cxx/pal/src/include/pal/palinternal.h
+++ chakracore-cxx/pal/src/include/pal/palinternal.h
@@ -307,7 +307,6 @@ function_name() to call the system's implementation
 #if HAVE__SNWPRINTF
 #undef _snwprintf
 #endif  /* HAVE__SNWPRINTF */
-#undef wcstod
 #undef _wcstoui64
 #undef _vsnprintf
 #undef _vsnwprintf
