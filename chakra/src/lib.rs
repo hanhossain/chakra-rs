54785218dceee1b462a763abff52dd653a7d86e0
Han Hossain <hanhossain@outlook.com>
2026-03-15 02:23:52 -0500

    PrintUsage to rust

--- chakra/src/lib.rs
+++ chakra/src/lib.rs
@@ -6,8 +6,21 @@ mod ffi {
 }
 
 fn print_usage() {
-    println!("\nUsage: ch [-v|-version] [-h|-help] [-?] [flaglist] <source file>");
-    println!("\t-v|-version\t\tDisplays version info");
-    println!("\t-h|-help\t\tDisplays this help message");
-    println!("\t-?\t\t\tDisplays this help message with complete [flaglist] info");
+    #[cfg(debug_assertions)]
+    {
+        println!("\nUsage: ch [-v|-version] [-h|-help] [-?] [flaglist] <source file>");
+        println!("\t-v|-version\t\tDisplays version info");
+        println!("\t-h|-help\t\tDisplays this help message");
+        println!("\t-?\t\t\tDisplays this help message with complete [flaglist] info");
+    }
+
+    #[cfg(not(debug_assertions))]
+    {
+        println!("\nUsage: ch [-v|-version] [-h|-help|-?] <source file>");
+        println!(
+            "Note: [flaglist] is not supported in Release builds; try a Debug or Test build to enable these flags."
+        );
+        println!("\t-v|-version\t\tDisplays version info");
+        println!("\t-h|-help|-?\t\tDisplays this help message");
+    }
 }
