d08c364b3e067e378f3694984c9e02540589f82b
Han Hossain <hanhossain@outlook.com>
2026-03-31 00:52:33 -0500

    PAL_fprintf(PAL_get_stderr()

--- chakracore-cxx/lib/Common/Memory/RecyclerWriteBarrierManager.cpp
+++ chakracore-cxx/lib/Common/Memory/RecyclerWriteBarrierManager.cpp
@@ -262,7 +262,7 @@ X64WriteBarrierCardTableManager::Initialize()
         void * cardTableSpace = ::VirtualAlloc(NULL, _cardTableNumEntries, MEM_RESERVE, PAGE_READWRITE);
         if (!cardTableSpace) // Crash Early with a meaningful message. Otherwise the behavior is undefined.
         {
-            PAL_fprintf(PAL_get_stderr(), "Out of Memory\n"); fflush(stderr);
+            fprintf(stderr, "Out of Memory\n"); fflush(stderr);
             abort();
         }
 
