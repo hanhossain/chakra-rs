c8e911dff838bf601b0ec0d685be36a5acf7e346
Han Hossain <hanhossain@outlook.com>
2026-04-02 12:16:28 -0500

    always generate chakra bindings

--- chakra/build.rs
+++ chakra/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let is_rust_analyzer = out_dir.contains("target/rust-analyzer");
-    let config = cxx_build::bridge("src/lib.rs");
-
-    if !is_rust_analyzer {
-        config.compile("chakra-binding");
-    }
+    cxx_build::bridge("src/lib.rs").compile("chakra-binding");
 }
