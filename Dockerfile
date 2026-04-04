e0f5339581fd0bd81470783c5577ec9032055fa4
Han Hossain <hanhossain@outlook.com>
2026-04-03 20:53:28 -0500

    fix docker build

--- Dockerfile
+++ Dockerfile
@@ -9,16 +9,14 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get update  \
     git \
     cmake \
     ninja-build \
-    clang-19 \
-    lld-19 \
-    gdb
+    clang-19 lld-19 \
+    gdb \
+    libc++-19-dev libc++abi-19-dev
 
-RUN ln -sf /usr/bin/gcc-14 /usr/bin/gcc
-RUN ln -sf /usr/bin/g++-14 /usr/bin/g++
-
-RUN ln -sf /usr/bin/clang++-19 /usr/bin/clang++
-RUN ln -sf /usr/bin/clang-19 /usr/bin/clang
+RUN update-alternatives --install /usr/bin/clang clang /usr/bin/clang-19 10
+RUN update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-19 10
 
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 ENV CARGO_BUILD_TARGET=x86_64-unknown-linux-gnu
+ENV IN_DOCKER=true
--- chakra/build.rs
+++ chakra/build.rs
@@ -1,3 +1,17 @@
+use std::path::PathBuf;
+
 fn main() {
     cxx_build::bridge("src/lib.rs").compile("chakra-binding");
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let target = std::env::var("TARGET").unwrap();
+    if !out_dir.contains(&target) {
+        let cxx_header = format!("{out_dir}/cxxbridge/include/rust/cxx.h");
+        let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+        let docker_cxx_header =
+            PathBuf::from(format!("{}/../target/docker/rust/cxx.h", manifest_dir));
+        dbg!(&cxx_header, &docker_cxx_header);
+        std::fs::create_dir_all(docker_cxx_header.parent().unwrap()).unwrap();
+        std::fs::copy(cxx_header, docker_cxx_header).unwrap();
+    }
 }
