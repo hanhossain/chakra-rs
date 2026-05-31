89f71c6469bb93c30e443af92aa09c07aeed6ca7
Han Hossain <hanhossain@outlook.com>
2026-05-31 03:31:50 -0500

    remove tt arguments from arg array before passing them to c++

--- chakra/src/lib.rs
+++ chakra/src/lib.rs
@@ -22,14 +22,15 @@ pub struct ChakraArgs {
 }
 
 impl ChakraArgs {
-    pub fn new(args: Vec<String>) -> Option<Self> {
+    pub fn new(args: &mut Vec<String>) -> Option<Self> {
         if args.len() < 2 {
             return None;
         }
 
         let mut chakra_args = ChakraArgs::default();
+        let mut tt_indices = Vec::new();
 
-        for arg in &args {
+        for (index, arg) in args.iter().enumerate() {
             if arg == "-v" || arg == "--version" {
                 return Some(ChakraArgs {
                     version: true,
@@ -48,20 +49,29 @@ impl ChakraArgs {
                 chakra_args.do_tt_record = true;
                 let record_uri = arg.split('=').next().unwrap_or_default().to_owned();
                 chakra_args.tt_uri = get_ttd_directory(record_uri);
+                tt_indices.push(index);
             } else if arg.starts_with("-TTReplay=") {
                 chakra_args.do_tt_replay = true;
                 let record_uri = arg.split('=').next().unwrap_or_default().to_owned();
                 chakra_args.tt_uri = get_ttd_directory(record_uri);
+                tt_indices.push(index);
             } else if arg.starts_with("-TTSnapInterval=") {
                 let raw_value = arg.split('=').next().unwrap_or_default();
                 chakra_args.tt_snap_interval = raw_value.parse::<u32>().unwrap_or(u32::MAX);
+                tt_indices.push(index);
             } else if arg.starts_with("-TTHistoryLength=") {
                 let raw_value = arg.split('=').next().unwrap_or_default();
                 chakra_args.tt_snap_history_length = raw_value.parse::<u32>().unwrap_or(u32::MAX);
+                tt_indices.push(index);
             } else if arg.starts_with("-TTDStartEvent=") {
                 let raw_value = arg.split('=').next().unwrap_or_default();
                 chakra_args.ttd_start_event_count = raw_value.parse::<u32>().unwrap_or(1);
+                tt_indices.push(index);
             }
+        }
+
+        for index in tt_indices.into_iter().rev() {
+            args.remove(index);
         }
 
         Some(chakra_args)
